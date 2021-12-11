02f5aeaa80c128e064661f1d6adcd350ed33f619
OLIPLICHE <olipliche@gmail.com>
2021-12-11 20:03:53 +0100

    typo mistake

--- db/migrate/20211211170003_create_posts.rb
+++ db/migrate/20211211170003_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.string :string
+      t.string :title
       t.text :text
       t.integer :comments_counter
       t.integer :likes_counter
