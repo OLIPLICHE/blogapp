619220ce3d492ee2b8bcd8b904855e56e296a4e2
OLIPLICHE <olipliche@gmail.com>
2021-12-14 10:16:58 +0100

    Fix Linters errors

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
-    belongs_to :author, foreign_key: 'User'
-    belongs_to :post, foreign_key: 'post_id'
+  belongs_to :author, foreign_key: 'User'
+  belongs_to :post, foreign_key: 'post_id'
 
   def update_likes_counter
     post.increment!(:likes_counter)
