86e244cb3d65c0b0e2d0cb96a2341b1f9d8166c0
OLIPLICHE <olipliche@gmail.com>
2021-12-17 06:01:31 +0100

    fix linters error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all 
+    @users = User.all
   end
 
   def show
-     @user = User.find(params[:id])
-     @posts = @user.recent_posts
+    @user = User.find(params[:id])
+    @posts = @user.recent_posts
   end
 end
