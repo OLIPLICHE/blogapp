c6db839f3b5ea4cfe3b2117778829246787acaf5
OLIPLICHE <olipliche@gmail.com>
2021-12-18 11:25:30 +0100

    Add create class in post controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,21 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:id])
+    @post.title = params[:title]
+    @post.text = params[:text]
+    @post.author_id = current_user.id
+    if @post.save
+      flash[:notice] = 'Post successfully created'
+      redirect_to user_posts_path
+    else
+      render 'new'
+    end
+  end
 end
