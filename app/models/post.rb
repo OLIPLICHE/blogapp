d59b4a8301f6a23a869637714a16928684f68b07
OLIPLICHE <olipliche@gmail.com>
2021-12-18 17:12:24 +0100

    Add validation for user models and post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,11 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: 'post_id'
   has_many :likes, foreign_key: 'post_id'
 
+  validates :Title, presence: true
+  validates :Text, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_ineger: true, geater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_ineger: true, geater_than_or_equal_to: 0 }
+
   def update_post_counter
     user.increment!(:posts_counter)
   end
