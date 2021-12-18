450a0194ff1c0fc3b40d503dbdf73ce9fc2827fa
OLIPLICHE <olipliche@gmail.com>
2021-12-18 12:11:50 +0100

    Update route and styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'users#index'
   resources :users, only: [:index, :show] do
-  resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :commnets, only: [:new, :create]
  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    end
   end
 end
