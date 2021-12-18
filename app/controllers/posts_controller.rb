d93d25746147f874fb89b61f9874294068464435
OLIPLICHE <olipliche@gmail.com>
2021-12-19 00:39:39 +0100

    fix linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,6 @@ class PostsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.includes(:comments, :likes).find(params[:id])
-
   end
 
   def new
